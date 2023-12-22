3275b14f3f00d807c5819db66006226a65244261
Tomer <tomerrim@gmail.com>
2023-12-22 09:20:44 +0200

    revalidate meals path when saving new meal and added metadata to the pages

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -3,6 +3,15 @@ import Image from "next/image";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
 
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+  if (!meal) notFound();
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 export default function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
 
--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,9 +1,14 @@
+"use client";
+
+import { useFormState } from "react-dom";
 import { shareMeal } from "@/lib/actions";
 import ImagePicker from "@/components/Meals/ImagePicker";
 import MealsFormSubmit from "@/components/Meals/MealsFormSubmit";
 import classes from "./page.module.css";
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, { message: null })
+
   return (
     <>
       <header className={classes.header}>
@@ -14,7 +19,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -43,6 +48,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your Image" name="image"/>
+          {state.message && <p className={classes.error}>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit/>
           </p>
